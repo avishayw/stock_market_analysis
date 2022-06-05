04c6d4f79bf3a0ffe4c7ee412939b0c8af335635
Avishay Wasse <av2558@gmail.com>
2022-06-05 20:11:40 +0300

    method_analysis_improved added some prints and a more detailed file for better UX

--- method_analysis_improved.py
+++ method_analysis_improved.py
@@ -12,11 +12,15 @@ sl_list = [2.0, 5.0, 10.0]
 tp_list = [2.0, 3.0, 5.0, 10.0, 20.0, 30.0]
 
 method_results = {}
+method_detailed_results = {}
 
 for tp in tp_list:
     method_results[tp] = {}
+    method_detailed_results[tp] = {}
     for sl in sl_list:
-        total_summary = {"Best case profits ratio": [],
+        print(f"Exploring for take profit {tp}% and stop loss {sl}%")
+        total_summary = {"Stock": [],
+            "Best case profits ratio": [],
                        "Worst case profits ratio": [],
                        "Best case losses ratio": [],
                        "Worst case losses ratio": [],
@@ -27,13 +31,16 @@ for tp in tp_list:
                        "Total exploration time": []}
 
         for ticker in snp_tickers:
+            print(f"Exploring for {ticker}")
             try:
                 result = doji_method(ticker, sl, tp)
+                total_summary["Stock"].append(ticker)
                 for key in total_summary.keys():
                     total_summary[key].append(result[key])
             except TypeError:
                 pass
 
+        method_detailed_results[tp][sl] = total_summary
         total_summary_avg = {}
         for key in total_summary:
             total_summary_avg[key] = [statistics.mean(total_summary[key])]
@@ -42,3 +49,5 @@ for tp in tp_list:
 
         with open("method_results.json", "w") as f:
             json.dump(method_results, f, indent=4)
+        with open("method_detailed_results.json", "w") as f:
+            json.dump(method_detailed_results, f, indent=4)
